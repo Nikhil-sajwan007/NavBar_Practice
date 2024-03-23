1b05b017a64182a1e27a45cdc33835889a6663b0
Nikhil Sajwan <147373447+Nikhil-sajwan007@users.noreply.github.com>
2024-03-23 05:50:01 +0000

    new-changes

---
+++ main.js
@@ -0,0 +1,10 @@
+// let x = document.getElementsById("dropMenu");
+
+
+// x.addEventListener('click', function() {
+
+//     let y = getElementsById("dropMenu");
+
+//     y.style.display = 'block';
+
+// });
