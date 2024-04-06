8c44d5dde5109bd874fe25cad7fa096164efbbbb
Nikhil Sajwan <147373447+Nikhil-sajwan007@users.noreply.github.com>
2024-04-06 06:23:35 +0000

    js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 let menuList = document.getElementById("menuList");
+let icon = document.getElementById("icon");
+let iconName = icon.getAttribute("name")
+
 // let hamMenu = document.getElementById("hamMenu");
 // console.log(hamMenu);
-let icon = document.getElementById("icon");
 // let iconName = icon.name;
 
-let iconName = icon.getAttribute("name")
 // console.log(iconName, "iconName"); 
 
-console.log(icon, "icon");
-console.log(iconName, "iconName");
+// console.log(icon, "icon");
+// console.log(iconName, "iconName");
 
 icon.addEventListener("click", ()=>{
-    console.log("okok")
+    // console.log("okok")
 
 
     if(iconName==="menu"){
