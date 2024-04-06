fcc75b3190db367e6d72df2d460e10d8e96f2b81
Nikhil Sajwan <147373447+Nikhil-sajwan007@users.noreply.github.com>
2024-04-06 06:21:10 +0000

    menu-changes-mobile

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
-
-
 let menuList = document.getElementById("menuList");
+// let hamMenu = document.getElementById("hamMenu");
+// console.log(hamMenu);
+let icon = document.getElementById("icon");
+// let iconName = icon.name;
+
+let iconName = icon.getAttribute("name")
+// console.log(iconName, "iconName"); 
+
+console.log(icon, "icon");
+console.log(iconName, "iconName");
+
+icon.addEventListener("click", ()=>{
+    console.log("okok")
 
 
-function openNow(){
+    if(iconName==="menu"){
 
-    menuList.style.display = "block";
+        icon.setAttribute("name", "close")
+        menuList.style.display = "block";
 
-}
+        // console.log("okok HEllo")
+        // iconName.valueOf = "close"
+        // console.log(iconName, "iconName"); 
+        
+    }else{
+
+        
+       
+
+    }
+
+});
+
+
+
+
+
+
+// function openNow(){
+//     
+// }
+
+
+
+
 
