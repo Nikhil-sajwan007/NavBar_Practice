5b4e4628b822ef91fe7c7a72c8199bd423c7a57b
Nikhil Sajwan <147373447+Nikhil-sajwan007@users.noreply.github.com>
2024-04-07 17:13:59 +0000

    code-update-22:43

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 let menuList = document.getElementById("menuList");
 let icon = document.getElementById("icon");
 
-// let hamMenu = document.getElementById("hamMenu");
-// console.log(hamMenu);
-// let iconName = icon.name;
-
-// console.log(iconName, "iconName"); 
-
-// console.log(icon, "icon");
-// console.log(iconName, "iconName");
-
 icon.addEventListener("click", ()=>{
     // console.log("okok")
     let iconName = icon.getAttribute("name");
@@ -25,9 +16,9 @@ icon.addEventListener("click", ()=>{
         
     }else if(iconName === "close"){
         icon.setAttribute("name", "menu");
-        console.log(iconName,"okok close")
+        // console.log(iconName,"okok close")
         menuList.style.display = "none";
-        menuList.style.top = "-13rem";
+        menuList.style.top = "100rem";
         
        
 
